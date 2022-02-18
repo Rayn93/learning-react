f4afcea486ba8eb8e7c658922d635e17942ced42
robert <robert.saternus@gmail.com>
2022-02-18 14:36:00 +0100

    Config project to use JSX

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const el = React.createElement(
-    'h2',
-    null,
-    'Lift History with React!',
-    React.createElement('span', null, 'My span element 2')
-);
+// const el = React.createElement(
+//     'h2',
+//     null,
+//     'Lift History with React!',
+//     React.createElement('span', null, 'Elo React!')
+// );
+
+const el = <h2>Lift History with React! <span>Elo React!</span></h2>
+
 console.log(el);
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
 
