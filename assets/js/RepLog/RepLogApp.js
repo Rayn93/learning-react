ddbec030fc81cd7d0146194509147ad5e788121b
robert <robert.saternus@gmail.com>
2022-02-18 16:07:07 +0100

    Add first React component - RepLogApp
    Add first React component - RepLogApp and organize js files.

---
+++ assets/js/RepLog/RepLogApp.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+//Base React Component
+export default class RepLogApp extends Component
+{
+    render() {
+        return <h2>Lift History with React! <span>Elo React!</span></h2>;
+    }
+}
--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,16 +1,7 @@
 import React from "react";
-import ReactDom from "react-dom";
+import {render} from "react-dom";
+import RepLogApp from './RepLog/RepLogApp';
 
-// const el = React.createElement(
-//     'h2',
-//     null,
-//     'Lift History with React!',
-//     React.createElement('span', null, 'Elo React!')
-// );
-
-const el = <h2>Lift History with React! <span>Elo React!</span></h2>
-
-console.log(el);
-ReactDom.render(el, document.getElementById('lift-stuff-app'));
+render(<RepLogApp/>, document.getElementById('lift-stuff-app'));
 
 
